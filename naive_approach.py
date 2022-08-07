31bb93ec5f9182d1ad2e73fd713cf622b07e26f0
Rebecca <82410393+rebecca-kleinbart@users.noreply.github.com>
2022-08-07 08:00:43 -0400

    Cost matrix is now imported from mels_teachers_rooms_weights

--- naive_approach.py
+++ naive_approach.py
@@ -1,47 +1,22 @@
+#add comments
+
+import time
 from itertools import permutations
 
+from draw_bipartite import draw_bipartite_solution
+from subsection_of_matrix import subsection_matrix
+from dictionary_to_list import dict_of_weights_to_list_of_lists_of_weights as dict_to_list
+from mels_teachers_rooms_weights import G, teachers, rooms
+
+
+
 def main():
+    #m = the size of the input
+    m = 9
+    print("m =", m)
+    cost = subsection_matrix(m)
 
-    score_matrix = [
-    [100, 20, 120, 0],
-    [0, 20, 120, 0],
-    [100, 20, 120, 0],
-    [100, 20, 120, 0],
-    ]
-
-    '''score_matrix = [
-    [20, 20, 20, 20, 0, 20, 100, 20, 120, 0, 20, 20, 20, 20, 20, 20, 20, 0, 120, 120, 80, 120, 0, 20, 20, 80, 20, 120],
-    [20, 20, 20, 20, 0, 20, 0, 20, 120, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 120, 80, 120, 0, 20, 20, 80, 20, 120],
-    [20, 20, 20, 20, 0, 20, 100, 20, 120, 0, 20, 20, 20, 0, 20, 20, 20, 20, 120, 120, 80, 120, 0, 20, 20, 80, 20, 120],
-    [20, 20, 20, 20, 0, 20, 100, 20, 120, 0, 0, 20, 0, 20, 20, 20, 20, 20, 120, 120, 80, 120, 0, 20, 20, 80, 20, 120],
-    [20, 20, 20, 0, 0, 20, 100, 20, 120, 0, 20, 20, 20, 20, 0, 20, 20, 20, 120, 120, 80, 120, 0, 20, 20, 80, 20, 120],
-    [20, 20, 20, 20, 0, 20, 100, 20, 120, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 120, 80, 120, 0, 20, 20, 80, 0, 120],
-    [20, 20, 20, 20, 0, 20, 100, 20, 120, 0, 20, 0, 20, 20, 20, 20, 20, 20, 120, 120, 80, 120, 0, 20, 20, 80, 20, 120],
-    [20, 20, 20, 20, 0, 20, 100, 0, 120, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 120, 80, 120, 0, 20, 20, 80, 20, 120],
-    [20, 0, 20, 20, 0, 20, 100, 20, 120, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 120, 80, 120, 0, 20, 20, 80, 20, 120],
-    [20, 20, 20, 20, 0, 20, 100, 20, 120, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 120, 80, 120, 0, 20, 0, 80, 20, 120],
-    [20, 20, 20, 20, 0, 20, 100, 20, 120, 0, 20, 20, 20, 20, 20, 20, 0, 20, 120, 120, 80, 120, 0, 20, 20, 80, 20, 120],
-    [20, 20, 20, 20, 0, 20, 100, 20, 120, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 120, 80, 120, 0, 0, 20, 80, 20, 120],
-    [20, 20, 20, 20, 0, 20, 100, 20, 120, 0, 20, 20, 20, 20, 20, 0, 20, 20, 120, 120, 80, 120, 0, 20, 20, 80, 20, 120],
-    [20, 20, 0, 20, 0, 20, 100, 20, 120, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 120, 80, 120, 0, 20, 20, 80, 20, 120],
-    [20, 20, 20, 20, 0, 20, 100, 20, 120, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 120, 0, 120, 0, 20, 20, 20, 20, 120],
-    [20, 20, 20, 20, 0, 20, 100, 20, 0, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 20, 60, 120, 0, 20, 20, 60, 20, 20],
-    [20, 20, 20, 20, 0, 20, 100, 20, 120, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 120, 60, 120, 0, 20, 20, 0, 20, 120],
-    [20, 20, 20, 20, 0, 20, 100, 20, 20, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 20, 60, 120, 0, 20, 20, 60, 20, 0],
-    [20, 20, 20, 20, 0, 0, 100, 20, 20, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 0, 60, 120, 0, 20, 20, 60, 20, 20],
-    [20, 20, 20, 20, 0, 20, 100, 20, 120, 0, 20, 20, 20, 20, 20, 20, 20, 20, 120, 120, 80, 120, 0, 20, 20, 80, 20, 120],
-    [40, 40, 40, 40, 0, 40, 120, 40, 140, 20, 20, 20, 40, 40, 40, 40, 40, 40, 0, 140, 100, 20, 20, 40, 40, 100, 40, 140],
-    [40, 40, 40, 40, 0, 40, 120, 40, 140, 20, 20, 20, 40, 40, 40, 40, 40, 40, 20, 140, 100, 0, 20, 40, 40, 100, 40, 140],
-    [60, 60, 60, 60, 0, 60, 140, 60, 160, 40, 60, 60, 60, 60, 60, 60, 60, 60, 160, 160, 120, 160, 60, 60, 60, 120, 60, 160],
-    [60, 60, 60, 60, 0, 60, 140, 60, 160, 40, 60, 60, 60, 60, 60, 60, 60, 60, 160, 160, 120, 160, 60, 60, 60, 120, 60, 160],
-    [60, 60, 60, 60, 0, 60, 140, 60, 160, 40, 60, 60, 60, 60, 60, 60, 60, 60, 160, 160, 120, 160, 60, 60, 60, 120, 60, 160],
-    [60, 60, 60, 60, 0, 60, 140, 60, 160, 40, 60, 60, 60, 60, 60, 60, 60, 60, 160, 160, 120, 160, 60, 60, 60, 120, 60, 160],
-    [60, 60, 60, 60, 0, 60, 140, 60, 160, 40, 60, 60, 60, 60, 60, 60, 60, 60, 160, 160, 120, 160, 60, 60, 60, 120, 60, 160],
-    [60, 60, 60, 60, 0, 60, 140, 60, 160, 40, 60, 60, 60, 60, 60, 60, 60, 60, 160, 160, 120, 160, 60, 60, 60, 120, 60, 160]]
-    '''
-
-
-    m = len(score_matrix)
+    m = len(cost)
 
     #create a list called score_sums to add sums and assignments to add orderings
     score_sums = []
@@ -49,50 +24,54 @@ def main():
     value_of_assignment = 0
     mins = []
     mins_permutations = []
-    # enumerate all the permutations of the numbers 0 - (len(score_matrix)-1)
+    list_of_solutions = []
+
+    # enumerate all the permutations of the numbers 0 - (len(cost)-1), in other words all the permutations of the numbers 0 through the length of the cost matrix.
     for assignment in permutations(range(m)):
         list_assignment = list(assignment)
         assignments.append(list_assignment)
         #Add to the list named "score_sums" the sum of the values for each possible permutation
         for i, j in enumerate(assignment):
-            value_of_assignment += score_matrix[i][j]
+            value_of_assignment += cost[i][j]
+
+        #Add the total weight cost for this particular assignment to the list score_sums. Later we'll identify the minimum total score/cost.
         score_sums.append(value_of_assignment)
         value_of_assignment = 0
-        print("Here is score_sums: ", score_sums)
-    print("Here is the whole list, score_sums: ", score_sums)
 
-    minSum = min(score_sums)
-    print("here is the minimum sum: ", minSum)
+    total_cost = min(score_sums)
+    print("here is the minimum sum: ", total_cost)
 
-    score_sums.count(minSum)
+    score_sums.count(total_cost)
 
     for i, num in enumerate(score_sums):
-        if num == minSum :
+        if num == total_cost :
             print("The minimum is the ", i, "th permutation", sep = '')
             print("And its value is", num)
             print(assignments[i])
             mins.append(i)
             mins_permutations.append(assignments[i])
 
-    teachers = ['Ali','Archaga','Baxter','Bright', 'Cas', 'Ellis', 'Farnham', 'Gary', 'Gostomski', 'Hendrick', 'Hurth', 'King', 'Kleinbart', 'Lotti', 'Masco', 'Maxfield', 'Morales', 'Newberger', 'Priestley', 'Robins', 'Rosenberg', 'Satriano', 'Sewall', 'Stern', 'Wallace', 'Winter', 'Wolff','Zaino']
-    rooms = ['0072','0089','1007', '1101','1103','2067','2071','2073','2075','2077','2101','2106','3071','3073','3077','4068A','4068B','4073','4077','4078','GymA','GymB','Float1','Float2','Float3','Float4','Float5','Float6']
-
 
     #printing out optimal solutions
     for assignment in mins_permutations:
+        solution = []
         for i, j in enumerate(assignment):
             for x in range(len(assignment)):
                 temp_teacher = teachers[x]
                 temp_room = rooms[assignment[x]]
-                print(temp_teacher, ":", temp_room )
+
+                temp_tuple = (temp_teacher, temp_room,G[temp_teacher][temp_room])
+                solution.append(temp_tuple)
+                draw_bipartite_solution(solution, "Bipartite Graph for Naive Approach")
+
+        list_of_solutions.append(solution)
+    print("Here is a list of all the solutions: ", list_of_solutions)
+    number_of_solutions = len(list_of_solutions)
+    print("There are ", number_of_solutions, "optimal assignment solutions")
+    return list_of_solutions,total_cost,
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     main()
-
-#solution =  min(
-    #sum(score_matrix[i][j] for i, j in enumerate(assignment))
-    #for assignment in permutations(range(m))
-#)
-
-#print("Here is the/an optimal solution:", solution)
+    print("--- %s seconds ---" % (time.time() - start_time))
