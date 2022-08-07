27e5cff73e661b1e47ca324167d85028d7c151bc
Rebecca <82410393+rebecca-kleinbart@users.noreply.github.com>
2022-08-07 07:34:35 -0400

    Added comment to dictionary_to_list

--- dictionary_to_list.py
+++ dictionary_to_list.py
@@ -1,13 +1,15 @@
+""" This function takes a cost dictionary as its input (A dictionary of dictionaries with the outer key being teachers and the outer values being a dictionary with the inner key being rooms and the inner values being weights/costs).
+    It outputs a cost matrix, a list of lists. Each list represents the costs of one teacher, for each room. """
+
 def dict_of_weights_to_list_of_lists_of_weights(cost_dict):
+    #create a list called 'costs'
     costs = []
+    #loop over inputted dictionary and create a list of costs for a particular teacher. Then add that entire list to costs.
     for teacher, rooms in cost_dict.items():
-        print("\nName:", teacher)
         values = []
         for weight in rooms:
-            print(weight + ':', rooms[weight])
             values.append(rooms[weight])
         costs.append(values)
-    print(costs)
 
     return(costs)
 
