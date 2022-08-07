5a8acd9c38122f1f41714862819c0a8566e6afe1
Rebecca <82410393+rebecca-kleinbart@users.noreply.github.com>
2022-08-07 08:40:14 -0400

    fix indentation inconsistency

--- order_variables.py
+++ order_variables.py
@@ -1,3 +1,3 @@
 def order_variables(cost_matrix):
     """ Function orders cost matrix by greatet sum to least sum"""
-  return sorted(cost_matrix,key=sum, reverse = True)
+    return sorted(cost_matrix,key=sum, reverse = True)
